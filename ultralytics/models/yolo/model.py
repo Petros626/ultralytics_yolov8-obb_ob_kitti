e773b59e6c2117aea1841bed5cd3ef99467793d0
Petros626 <petros.katsoulakos@web.de>
2024-12-31 19:06:58 +0100

    add print for debugging

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -13,6 +13,7 @@ class YOLO(Model):
 
     def __init__(self, model="yolo11n.pt", task=None, verbose=False):
         """Initialize YOLO model, switching to YOLOWorld if model filename contains '-world'."""
+        print(f'class YOLO: current loaded model is: {model}')
         path = Path(model)
         if "-world" in path.stem and path.suffix in {".pt", ".yaml", ".yml"}:  # if YOLOWorld PyTorch model
             new_instance = YOLOWorld(path, verbose=verbose)
@@ -21,6 +22,7 @@ class YOLO(Model):
         else:
             # Continue with default YOLO initialization
             super().__init__(model=model, task=task, verbose=verbose)
+        print(f'class YOLO: current task is:', task)
 
     @property
     def task_map(self):
