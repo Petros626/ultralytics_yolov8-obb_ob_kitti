21884fc12456d3e0ca3245051300ab31752dd6f4
Petros626 <petros.katsoulakos@web.de>
2025-01-25 16:27:40 +0100

    replace _get_session (ray 2.3.1) with get_session (ray 2.41.0)

--- ultralytics/utils/callbacks/raytune.py
+++ ultralytics/utils/callbacks/raytune.py
@@ -14,7 +14,8 @@ except (ImportError, AssertionError):
 
 def on_fit_epoch_end(trainer):
     """Sends training metrics to Ray Tune at end of each epoch."""
-    if ray.train._internal.session._get_session():  # replacement for deprecated ray.tune.is_session_enabled()
+    #if ray.train._internal.session._get_session():  # replacement for deprecated ray.tune.is_session_enabled() <- use when ray v2.3.1
+    if ray.train._internal.session.get_session():   # v2.41.0 replacement for depreacted _get_session() <- use when ray v2.41.0
         metrics = trainer.metrics
         session.report({**metrics, **{"epoch": trainer.epoch + 1}})
 
