589d2adf9fa547881ece45077105eff5e9ee7630
Petros626 <petros.katsoulakos@web.de>
2026-02-19 18:12:21 +0100

    add comment rboxes.append

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -354,7 +354,7 @@ def xyxyxyxy2xywhr(x):
         # NOTE: Use cv2.minAreaRect to get accurate xywhr,
         # especially some objects are cut off by augmentations in dataloader.
         (cx, cy), (w, h), angle = cv2.minAreaRect(pts)
-        rboxes.append([cx, cy, w, h, angle / 180 * np.pi])
+        rboxes.append([cx, cy, w, h, angle / 180 * np.pi]) # degree -> rad
     return torch.tensor(rboxes, device=x.device, dtype=x.dtype) if is_torch else np.asarray(rboxes)
 
 
